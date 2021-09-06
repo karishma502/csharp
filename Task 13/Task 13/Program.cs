9a69126169df9e87de8a5ddfc6b103622f25910f
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-06 20:33:31 +0530

    Update Program.cs

--- Task 13/Task 13/Program.cs
+++ Task 13/Task 13/Program.cs
@@ -44,12 +44,12 @@ namespace Task_13
 
             //Console.Read();
 
-            //FiveEmpDetail();
+            FiveEmpDetail();
 
             ITEmployee t = new ITEmployee();
 
-            t.skillsemp();
-            t.CalculateBonus();
+            //t.skillsemp();
+           // t.CalculateBonus();
         }
 
        
@@ -62,22 +62,22 @@ namespace Task_13
         /// </summary>
 
         //private static void FiveEmpDetail()
-        //{
-        //    List<Employee> emp = new List<Employee>();
-        //    emp.Add(new Employee { id = 101, name = "sumit", age = 23, salary = 4000 });
-        //    emp.Add(new Employee { id = 102, name = "kiran", age = 24, salary = 6000 });
-        //    emp.Add(new Employee { id = 103, name = "suman", age = 25, salary = 7000 });
-        //    emp.Add(new Employee { id = 104, name = "raman", age = 26, salary = 9000 });
-        //    emp.Add(new Employee { id = 105, name = "samir", age = 26, salary = 9000 });
-        //
-        //
-        //    foreach (var Employee in emp)
-        //    {
-        //        Console.WriteLine("id: {0}, name:{1}, age: {2}, salary: {3}", Employee.id, Employee.name, Employee.age, Employee.salary);
-        //    }
-        //    Console.ReadLine();
-        //
-        //
-        //}
+        {
+            List<Employee> emp = new List<Employee>();
+            emp.Add(new Employee { id = 101, name = "sumit", age = 23, salary = 4000 });
+            emp.Add(new Employee { id = 102, name = "kiran", age = 24, salary = 6000 });
+            emp.Add(new Employee { id = 103, name = "suman", age = 25, salary = 7000 });
+            emp.Add(new Employee { id = 104, name = "raman", age = 26, salary = 9000 });
+            emp.Add(new Employee { id = 105, name = "samir", age = 26, salary = 9000 });
+        
+        
+            foreach (var Employee in emp)
+            {
+                Console.WriteLine("id: {0}, name:{1}, age: {2}, salary: {3}", Employee.id, Employee.name, Employee.age, Employee.salary);
+            }
+            Console.ReadLine();
+        
+        
+        }
     }
 }
