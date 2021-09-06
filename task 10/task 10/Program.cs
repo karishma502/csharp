259209d9a7290fd11d3705e86a02901ca27a3ebc
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-06 12:39:33 +0530

    Add files via upload

---
+++ task 10/task 10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] StuID = new int[10];
+            string[] FullName = new string[100];
+            string[] CourseName = new string[100];
+            string[] PhoneNum = new string[10];
+            int vall = 0;
+            Console.WriteLine("Enter how many entries you want to enter:");
+            vall = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < vall; i++)
+            {
+                Console.WriteLine("Enter StudentID:");
+                int ID = Convert.ToInt32(Console.ReadLine());
+                StuID[i] = ID;
+                Console.WriteLine("Enter FullName:");
+                string FN = Console.ReadLine();
+                FullName[i] = FN;
+                Console.WriteLine("Enter CourseName:");
+                string CN = Console.ReadLine();
+                CourseName[i] = CN;
+                Console.WriteLine("Enter PhoneNo:");
+                string PN = Console.ReadLine();
+                PhoneNum[i] = PN;
+            }
+
+            Console.WriteLine("Students data entered by user are...");
+
+            for (int i = 0; i < vall; i++)
+            {
+                Console.WriteLine("ID: {0}", StuID[i]);
+                Console.WriteLine("FullName: {0}", FullName[i]);
+                Console.WriteLine("CourseName: {0}", CourseName[i]);
+                Console.WriteLine("PhoneNo: {0}", PhoneNum[i]);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ task 9/task 9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] student = new string[5];
+            string name;
+            try
+            {
+                Console.WriteLine("Enter Student Names:");
+
+                for (int i = 0; i <= 4; i++)
+                {
+                    name = Console.ReadLine();
+                    student[i] = name;
+                }
+
+                Console.WriteLine("Array of students entered by user..... ");
+
+                for (int i = 0; i <= 4; i++)
+                {
+                    Console.WriteLine(student[i]);
+
+                }
+
+           
+
+                name = Console.ReadLine();
+                student[6] = name;
+
+
+            }
+
+            catch (IndexOutOfRangeException err)
+            {
+
+                Console.WriteLine(err.Message);
+
+            }
+
+            catch (Exception)
+            {
+
+                Console.WriteLine("error - array size is full, exception is handled");
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
