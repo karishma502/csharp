bb90434a50a1323d2d2d2e22f0ce09b4f2552c7c
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-06 12:47:38 +0530

    Add files via upload

---
+++ task 12/task 12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_12
+{
+    class samplee
+    {
+        public static void IFutureMethod()
+        {
+            Console.WriteLine("IFutureMethod method");
+        }
+        public void ZensarMethod(string v)
+        {
+            first();
+            Console.WriteLine("ZensarMethod method");
+        }
+
+        public void first()
+        {
+
+        }
+
+        public void ZensarMethod(int i)
+        {
+            Console.WriteLine(i);
+            ZensarMethod();
+        }
+
+        private void ZensarMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            samplee obj = new samplee();
+            obj.first();
+            obj.ZensarMethod("Akkki");
+            Console.Read();
+        }
+    }
+}
