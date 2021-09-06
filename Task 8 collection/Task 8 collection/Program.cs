1b1be6c951c6ed49ecd0743cb1d2bad9d891ee5d
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-06 12:36:26 +0530

    Add files via upload

---
+++ Task 8 collection/Task 8 collection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add("Sumit");
+            arr.Add(19);
+            arr.Add(true);
+
+            //var stringg = arr[0];
+            //Console.WriteLine(stringg);
+
+            //var intt = arr[1];
+            //Console.WriteLine(intt);
+
+
+            //var booleann = arr[2];
+            //Console.WriteLine(booleann);
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+           
+
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task 7/task 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            NamesDisplay("karishma", "miraj");
+            NamesDisplay("sayli", "janhvi", "sangli", "kokan", "01");
+
+            Console.Read();
+
+        }
+
+        private static void NamesDisplay(params string[] parameters)
+        {
+            Console.WriteLine("Employee Details are as follows.....");
+
+            foreach (var item in parameters)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+    
+}
