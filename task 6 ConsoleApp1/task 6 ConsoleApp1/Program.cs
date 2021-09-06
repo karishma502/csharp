1bf466988cb29dfd0b49a670d2a37b255e823d61
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-06 12:26:18 +0530

    Add files via upload

---
+++ task 6 ConsoleApp1/task 6 ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student s = new student();
+            s.getadmission();
+
+        }
+    }
+}
