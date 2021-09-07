cc29d425647f70f4445e1a853b31fe57daa88e7b
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-07 20:51:38 +0530

    Add files via upload

---
+++ task 17/task 17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo u = new UserInfo("karishma.com","Admin",123);
+             Login();
+
+
+            void Login()
+            {
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Enter Username");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Enter Password");
+                int pass = Convert.ToInt32(Console.ReadLine());
+
+
+                if (u.UserName == username ||u.Password == pass ||  u.Email== email)
+                {
+                    Console.WriteLine("Welcome to portal...! have a nice day");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid login name or password. Please provide correct credentials");
+                }
+                Console.Read();
+            }
+           
+        }
+       
+    }
+}
---
+++ task 17/task 17/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_17
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+
+        public UserInfo(string email, string username,int password )
+        {
+            Email = email;
+            UserName = username;
+            Password = password;
+
+        }
+        
+
+    }
+}
