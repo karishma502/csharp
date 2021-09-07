d3dbbfe20f536c3ff7e3cd81fe2821e639c5970f
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-08 01:11:43 +0530

    Add files via upload

---
+++ task 16/task 16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\HP\source\repos\FileOperation\Text.txt";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Console.WriteLine("File has been Deleted");
+            }
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                Console.WriteLine("New File Recreated");
+                sw.WriteLine("Welcome to Zensar");
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                Console.WriteLine("Accessing existing file");
+                sw.WriteLine("Appending New file");
+            }
+
+
+            using (StreamReader rdr = File.OpenText(path))
+            {
+                Console.WriteLine("reading from file");
+                Console.WriteLine(rdr.ReadToEnd());
+            }
+            Console.ReadLine();
+
+        }
+    }
+    
+}
