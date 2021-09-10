82b2c1412d7730f1b8da1d4d67fdd758eb4b48d9
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-10 17:51:05 +0530

    Add files via upload

---
+++ task 14/task 14/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_14
+{
+    class Program
+    {
+        public interface IContractEmployee
+        {
+            void CalculateSalary(int hour, int perHourRupees);
+        }
+
+        public class Infosys : IContractEmployee
+        {
+            int salary;
+
+            public void CalculateSalary(int hour, int perHourRupees)
+            {
+                int hr, rup;
+                hr = hour;
+                rup = perHourRupees;
+
+                salary = hr * rup;
+
+                Console.WriteLine("salary is : "+ salary);
+                Console.ReadLine();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Infosys obj = new Infosys();
+            Console.WriteLine("Enter Hour");
+            int hr = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("enter perHourRupees");
+            int rup = Convert.ToInt32(Console.ReadLine());
+
+            obj.CalculateSalary(hr, rup);
+            Console.ReadKey();
+
+        }
+    }
+}
