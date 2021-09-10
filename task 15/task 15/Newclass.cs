b8bbe921960bd5dd962703090567a0b40c7bbfdc
Karishma Kate <83213536+karishma502@users.noreply.github.com>
2021-09-10 18:05:10 +0530

    Add files via upload

---
+++ task 15/task 15/Newclass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_15
+{
+    public class Newclass
+    {
+        // Method for Fibonacci series
+        public void Fibonacci_Rec(int length)
+        {
+            Fibonacci_Rec_1(0, 1, 1, length);
+        }
+
+        private void Fibonacci_Rec_1(int a, int b, int counter, int length)
+        {
+            if (counter <= length)
+            {
+                Console.Write("{0} ", a);
+                Fibonacci_Rec_1(b, a + b, counter + 1, length);
+            }
+        }
+
+
+  
+
+        public int Get_Nth_Fibonacci_Rec(int n)
+        {
+
+            if ((n == 0) || (n == 1))
+            {
+                return n;
+            }
+            else
+            {
+                return Get_Nth_Fibonacci_Rec(n - 1) + Get_Nth_Fibonacci_Rec(n - 2);
+            }
+        }
+    }
+}
---
+++ task 15/task 15/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_15
+{
+    public class Operation
+    {
+
+        // Palindrome String Program
+        private static bool IsPalRecursion(String str, int s, int l)
+        {
+            if (s == l)
+            {
+                return true;
+            }
+            if ((str[s] != str[l]))
+            {
+                return false;
+            }
+            if (s < l + 1)
+
+                return IsPalRecursion(str, s + 1, l - 1);
+            return true;
+        }
+
+        private static bool IsPalindrome(String str)
+        {
+            int n = str.Length;
+            if (n == 0)
+
+                return true;
+            return IsPalRecursion(str, 0, n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            String str = Console.ReadLine();
+            if (IsPalindrome(str))
+            {
+                Console.WriteLine("True");
+            }
+            else
+                Console.WriteLine("False");
+            Console.Read();
+
+
+
+            //call for fibonacci
+
+            Newclass c = new Newclass();
+            c.Fibonacci_Rec(11);
+            Console.ReadLine();
+            int val = 7;
+            int result = c.Get_Nth_Fibonacci_Rec(val);
+            Console.WriteLine("{0} th number in Fibonacci Series is = {1}", val, result);
+            Console.ReadLine();
+
+            // call for leap year using try catch
+            try
+            {
+                Console.WriteLine("Enter year:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                //int b = Convert.ToInt32(a);
+                CheckLeapYear(a);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+
+
+        private static void CheckLeapYear(int a)
+        {
+
+
+            if ((a % 4) == 0)
+            {
+                //return false;
+                Console.WriteLine("{0} is a Leap Year", a);
+            }
+
+            else
+
+                Console.WriteLine("Not a leap year");
+
+        }
+
+    }
+}
